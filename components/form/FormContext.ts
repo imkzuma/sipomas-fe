ad2793511a5285199d7798caec6f68de2c66eb45
imkzuma <edu.gungkrisna@gmail.com>
2023-03-31 23:35:31 +0800

    [ADDED] Added interface for context

--- components/form/FormContext.ts
+++ components/form/FormContext.ts
@@ -1,3 +1,9 @@
+import { PasswordContextInterface } from "@/utils/interfaces/form";
 import { createContext } from "react";
 
-export const PasswordContext = createContext<any>(null);
+export const PasswordContext = createContext<PasswordContextInterface>({ 
+    showPassword: false, 
+    setShowPassword: () => {}, 
+    showVerifyPassword: false, 
+    setShowVerifyPassword: () => {}
+});
