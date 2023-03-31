00904849d9f96c51fea6ab39763f51f56db83d10
imkzuma <edu.gungkrisna@gmail.com>
2023-03-31 23:34:25 +0800

    [ADDED] Added State management for future and Fixed some Bugs

--- components/auth/register/FormRegister.tsx
+++ components/auth/register/FormRegister.tsx
@@ -1,15 +1,25 @@
-import { useContext } from 'react';
+import Link from 'next/link';
+import { useContext, useState } from 'react';
 import { Button, Form, Stack } from "react-bootstrap";
 
 import { FormTextInput, FormTextPassword } from "@/components/form";
 import { PasswordContext } from "@/components/form/FormContext";
-import Link from 'next/link';
 
 export default function FormRegister(){
-    const { showPassword } = useContext(PasswordContext);
+    const { showPassword, showVerifyPassword } = useContext(PasswordContext);
+
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [verifyPassword, setVerifyPassword] = useState<string>("");
+
+    const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+        console.log(username, email, password, verifyPassword);
+    }
 
     return(
-        <Form>
+        <Form onSubmit = { handleSubmit }>
             <Stack
                 direction = "vertical"
                 gap = {5}
@@ -22,21 +32,48 @@ export default function FormRegister(){
                         controlId = "inputName"
                         label = "Username"
                     >
-                        <Form.Control type = "text" placeholder = "Input your username" required />
+                        <Form.Control 
+                            type = "text" 
+                            placeholder = "Input your username" 
+                            onChange = {(e) => setUsername(e.target.value)}
+                            required 
+                        />
                     </FormTextInput>
 
                     <FormTextInput
                         controlId = "inputEmail"
                         label = "Email Address"
                     >
-                        <Form.Control type = "email" placeholder = "Input your email" required />
+                        <Form.Control 
+                            type = "email" 
+                            placeholder = "Input your email" 
+                            onChange = {(e) => setEmail(e.target.value)}
+                            required 
+                        />
                     </FormTextInput>
 
                     <FormTextPassword
                         controlId = "inputPassword"
                         label = "Password"
                     >
-                        <Form.Control type = { showPassword? "password" : "text" } placeholder = "Input your password" required />
+                        <Form.Control 
+                            type = { showPassword? "text" : "password" } 
+                            placeholder = "Input your password" 
+                            onChange = {(e) => setPassword(e.target.value)}
+                            required 
+                        />
+                    </FormTextPassword>
+
+                    <FormTextPassword
+                        controlId = "inputVerifyPassword"
+                        label = "Re-Type Password"
+                    >
+                        <Form.Control 
+                            type = { showVerifyPassword? "text" : "password" } 
+                            placeholder = "Input your password" 
+                            onChange = {(e) => setVerifyPassword(e.target.value)}
+                            required 
+                        />
                     </FormTextPassword>
 
                 </Stack>
@@ -45,6 +82,7 @@ export default function FormRegister(){
                     <Button
                         variant="primary"
                         className = "p-3 fs-5"
+                        type = "submit"
                     >
                         Register
                     </Button>
