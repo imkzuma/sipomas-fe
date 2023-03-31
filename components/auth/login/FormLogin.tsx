d824354d75f6147bccbe26884b438af756910e80
imkzuma <edu.gungkrisna@gmail.com>
2023-03-31 23:33:03 +0800

    [FIXED] Fixed Bugs and Add State Management for future

--- components/auth/login/FormLogin.tsx
+++ components/auth/login/FormLogin.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button, Form, Stack } from "react-bootstrap";
 
 import { FormTextInput, FormTextPassword } from "@/components/form/index";
@@ -8,8 +8,16 @@ import { PasswordContext } from "@/components/form/FormContext";
 export default function FormLogin(){
     const { showPassword } = useContext(PasswordContext);
 
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(email,password);
+    }
+
     return(
-        <Form>
+        <Form onSubmit = { handleSubmit }>
             <Stack
                 direction = "vertical"
                 gap = {5}
@@ -22,30 +30,42 @@ export default function FormLogin(){
                         controlId = "inputEmail"
                         label = "Email Address"
                     >
-                        <Form.Control type = "email" placeholder = "youremail@email.com" required />
+                        <Form.Control 
+                            type = "email" 
+                            placeholder = "youremail@email.com" 
+                            onChange = {(e) => setEmail(e.target.value)}
+                            required 
+                        />
                     </FormTextInput>
 
                     <FormTextPassword
                         controlId="inputPassword"
                         label = "Password"
                     >
-                        <Form.Control type = { showPassword? "text" : "password" } placeholder = "Input your password" required />
+                        <Form.Control 
+                            type = { showPassword? "text" : "password" } 
+                            placeholder = "Input your password" 
+                            onChange = {(e) => setPassword(e.target.value)}
+                            required 
+                        />
                     </FormTextPassword>
-
-                    <Link href = "#" className = 'text-primary text-decoration-none'>
-                        Forgot Password ?
-                    </Link>
+                    <div>
+                        <Link href = "#" className = 'text-primary text-decoration-none'>
+                            Forgot Password ?
+                        </Link>
+                    </div>
                 </Stack>
                 
                 <Stack direction = "vertical" className = 'text-center' gap = {3}>
                     <Button
                         variant="primary"
+                        type="submit"
                         className = "p-3 fs-5"
                     >
                         Login
                     </Button>
                     
-                    <p className = 'ny-auto text-muted9o'>
+                    <p className = 'ny-auto text-muted'>
                         Don&apos;t have an account ? 
                         <Link href = "/auth/register" className = 'ps-1 text-decoration-none text-primary'>
                             Sign Up
