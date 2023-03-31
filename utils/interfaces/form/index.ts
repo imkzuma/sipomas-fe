15dbd32f8e9cc0d68358e6cd1b845f802303e64d
imkzuma <edu.gungkrisna@gmail.com>
2023-03-31 23:37:55 +0800

    [ADDED] Added interface for Form Context

--- utils/interfaces/form/index.ts
+++ utils/interfaces/form/index.ts
@@ -3,3 +3,10 @@ export interface FormInputInterface{
     label : string;
     children : any;
 }
+
+export interface PasswordContextInterface{
+    showPassword : boolean;
+    setShowPassword : (showPassword : boolean) => void;
+    showVerifyPassword : boolean;
+    setShowVerifyPassword : (showVerifyPassword : boolean) => void;
+}
